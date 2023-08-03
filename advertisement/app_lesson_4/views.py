75459e2f690c284d99ccda3774b265169eb5f968
favoyik <136617323+favoyik@users.noreply.github.com>
2023-08-04 00:50:05 +0300

    Add files via upload

---
+++ advertisement/app_lesson_4/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def fun(request):
+    return HttpResponse('Домашка по 4 занятию')
